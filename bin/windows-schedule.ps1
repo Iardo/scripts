40740a2b14e178be13950d36989e3815035c892d
Iván Ramos <iardoru@gmail.com>
2025-08-31 12:00:11 -0500

    update windows-schedule.ps1

--- bin/windows-schedule.ps1
+++ bin/windows-schedule.ps1
@@ -12,9 +12,10 @@ $task_user   = [Environment]::MachineName + '\' + $env:UserName
 $cfg_general = New-ScheduledTaskPrincipal -UserID $task_user -RunLevel Limited
 $cfg_action  = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $('-NoProfile -ExecutionPolicy Bypass -File ' + $script)
 $cfg_trigger = New-ScheduledTaskTrigger -AtLogon -User $task_user
+$cfg_setting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd
 
 if (-Not (Get-ScheduledTask -TaskName $task_name -ErrorAction SilentlyContinue)) {
     Write-Host $task_name '... ' -NoNewline
-    Register-ScheduledTask -TaskName $task_name -Description $task_desc -Principal $cfg_general -Trigger $cfg_trigger -Action $cfg_action | Out-Null
+    Register-ScheduledTask -TaskName $task_name -Description $task_desc -Principal $cfg_general -Trigger $cfg_trigger -Action $cfg_action -Settings $cfg_setting | Out-Null
     Write-Host 'done'
 }
